666a5dad702cb3838ea2dd2626f5f69ad1901626
Langdon Douglas <langdon.douglas26@gmail.com>
2024-09-15 11:36:59 -0500

    Added variables

---
+++ script.js
@@ -0,0 +1,12 @@
+//variables
+let depressed = null;
+let excited = null;
+let hangry = null;
+let happy = null;
+let hungry = null;
+let mad = null;
+let motivated = null;
+let nervous = null;
+let sad = null;
+let tired = null;
+
