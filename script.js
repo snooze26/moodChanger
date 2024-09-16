e180a10ccf21fca3baf3d9c838bbe9b1a25fde73
snooze26 <langdon.douglas26@gmail.com>
2024-09-16 14:37:28 -0500

    Added all background colors to all moods

--- script.js
+++ script.js
@@ -27,55 +27,55 @@ function moodSelector(mood) {
         case "excited":
             container.style.background = "radial-gradient(circle, #ff0000, #cc9900, #ffda47)";            
             message.textContent = "Things are looking good!!!";
-            message.style.color = "#ffffff";
+            message.style.color = "#ff4000";
             break;  
 
         case "hangry":
             container.style.background = "radial-gradient(circle, #b30000, #006600, #ffbf00)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
+            message.textContent = "HULK MAD!! FEED HULK!!";
             message.style.color = "#ffffff";
             break;
 
         case "happy":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #33cccc, #ffffff, #ff99cc)";            
+            message.textContent = "Lets have a good time! We only live so long.";
+            message.style.color = "#00b3b3";
             break;
 
         case "hungry":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #663300, #003300, #006600)";            
+            message.textContent = "Foooooooood pleeeeeeeeease";
+            message.style.color = "#ffffff";
             break;
 
         case "mad":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #b30000, #000000, #ff1a1a)";            
+            message.textContent = "Scream or beat up a pillow <3";
+            message.style.color = "#ffffff";
             break;
 
         case "motivated":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #99ffff, #99ff99, #ccffff)";            
+            message.textContent = "Let's get to it! Don't be the thing that holds you back!!";
+            message.style.color = "green";
             break;
             
         case "nervous":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #59b300, #b38600, #ffbf00)";            
+            message.textContent = "Take deep breaths, center yourself. Oosah. Oosah";
+            message.style.color = "#ffffff";
             break;
 
         case "sad":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #00264d, #404040)";            
+            message.textContent = "Things will get better. Try to stay as strong as you can. Take your time to feel your emotions";
+            message.style.color = "#ffffff";
             break;
 
         case "tired":
-            container.style.background = "radial-gradient(circle, #212136, gray)";            
-            message.textContent = "Things will get better just try to stay as strong as you can";
-            message.style.color = "Gray";
+            container.style.background = "radial-gradient(circle, #333333, #00001a)";            
+            message.textContent = "Go to sleep!";
+            message.style.color = "#ffffff";
             break;
 
     }
